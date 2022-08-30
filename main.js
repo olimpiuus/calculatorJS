f4229af2f89a9d4b8c6d014698e414a19f699754
olimpiuus <kolegin.yuriu@gmail.com>
2022-08-29 20:35:13 -0600

    finish project v1

--- main.js
+++ main.js
@@ -200,6 +200,7 @@ btnChangeOperant.addEventListener('click', () => {
     if (monitor.textContent.includes('result:')) {
         numberOne *= (-1)
         updateMonitor(`result:${numberOne}`)
+        numberTwo = undefined
         return
     }
     if (tempNumber == '') { tempNumber = '-' } else { tempNumber === '-' ? tempNumber = '' : tempNumber *= (-1) }
